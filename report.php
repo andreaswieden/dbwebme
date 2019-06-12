afa18e4779a990d385a275086dcb510d500347e0
Andreas Wieden <andreas.wieden@chalmers.se>
2019-06-12 13:22:46 +0200

    added content to report.php

---
+++ report.php
@@ -0,0 +1,34 @@
+<!doctype html>
+
+<html lang="sv">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <title>Me-sidan</title>
+  </head>
+
+  <body>
+  
+    <header class="site-header">
+      <img src="img/logo.jpg" alt="logo">
+      <span class="site-title">Me-Sida för Mikael</span>
+      <span class="site-slogan">Min första fina me-sida är på gång</span>
+    </header>
+  
+    <h1>Redovisning</h1>
+    <p>Här kommer snart min egen fina me sida</p>
+    <img src="img/me.jpg" class="me" alt="Bild på Roos">
+    
+    <footer>
+      <hr>
+      <a href="http://validator.w3.org/check/referer">HTML5</a>
+      <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>
+      <a href="http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance">UNICORN</a>
+    </footer>
+    
+  </body>
+</html>
+
+
+
