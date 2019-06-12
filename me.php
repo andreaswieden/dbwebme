1f9208a85b292dace80ebf4d84c327be80d05bfc
Andreas Wieden <andreas.wieden@chalmers.se>
2019-06-12 11:58:53 +0200

    added site-header, site-slogan and img to header in me.php

--- me.php
+++ me.php
@@ -8,6 +8,13 @@
   </head>
 
   <body>
+  
+    <header>
+      <img src="img/logo.jpg" alt="logo">
+      <span class="site-title">Me-Sida för Mikael</span>
+      <span class="site-slogan">Min första fina me-sida är på gång</span>
+    </header>
+  
     <h1>Om Mig Själv</h1>
     <p>Här kommer snart min egen fina me sida</p>
     <img src="img/me.jpg" class="me" alt="Bild på Roos">
