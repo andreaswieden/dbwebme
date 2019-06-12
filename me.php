2bb6769f82d6ccb91ee8fc5d83aeaeabe88f628e
Andreas Wieden <andreas.wieden@chalmers.se>
2019-06-12 14:50:25 +0200

    fixed path fault in include header and footer in file me.php

--- me.php
+++ me.php
@@ -1,14 +1,10 @@
-<?php
-  include( __DIR__ . "/incl/header.php");
-?>
+<?php include("incl/header.php"); ?>
     
   <h1>Om Mig Själv</h1>
   <p>Här kommer snart min egen fina me sida</p>
   <img src="img/me.jpg" class="me" alt="Bild på Roos">
     
-<?php
-  include( __DIR__ . "/incl/footer.php");
-?>
+<?php include("incl/footer.php"); ?>
 
 
 
