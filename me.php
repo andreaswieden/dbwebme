c64a5ac560e207b0a4ac60d517a160b4384f91fc
Andreas Wieden <andreas.wieden@chalmers.se>
2019-06-12 09:09:33 +0200

    added some basic content to me.php

---
+++ me.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html lang="sv">
+  <head>
+    <meta charset="utf-8">
+    <title>Me-sidan</title>
+  </head>
+
+  <body>
+    <h1>Om Mig Själv</h1>
+    <p>Här kommer snart min egen fina me sida</p>
+    <img src="img/me.jpg" class="me" alt="Bild på Roos">
+  </body>
+</html>
+
+
+
