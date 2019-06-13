1ebc7765390d625561d12c751eb0d1edb3e1ddb5
Andreas Wieden <andreas.wieden@chalmers.se>
2019-06-13 12:03:44 +0200

    added a basic byline content in incl/byline.php

---
+++ incl/byline.php
@@ -0,0 +1,8 @@
+<div class="byline">
+  
+  <img src="img/me-small.jpg" alt="bild på Mikael Roos">
+  <figcaption>Mikael Roos</figcaption>
+  <span class="byline-text">
+    Mikael Roos undervisar vid Blekinge Tekniska Högskola och skriver om webbprogrammering, webbutveckling och utmaningen med att undervisa på distans och campus. Mikael driver ett företag inom webbprogrammering och på kvällarna utvecklar han sina projekt inom öppen källkod.
+  </span>
+</div>
