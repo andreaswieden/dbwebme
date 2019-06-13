1c2ffc20409dec54e5fc26b710680e2a9eee9442
Andreas Wieden <andreas.wieden@chalmers.se>
2019-06-13 13:44:16 +0200

    fixed minor bugg with rekursive span usage, changed to div instead

--- incl/byline.php
+++ incl/byline.php
@@ -1,8 +1,8 @@
 <div class="byline">
-  <span class="byline-img-holder">
+  <div class="byline-img-holder">
     <img src="img/me-small.jpg" alt="bild på Mikael Roos">
-    <figcaption>Mikael Roos</figcaption>
-  </span>
+    <div class="byline-img-holder-caption">Mikael Roos</div>
+  </div>
   
   <span class="byline-text">
     Mikael Roos undervisar vid Blekinge Tekniska Högskola och skriver om webbprogrammering, webbutveckling och utmaningen med att undervisa på distans och campus. Mikael driver ett företag inom webbprogrammering och på kvällarna utvecklar han sina projekt inom öppen källkod.
