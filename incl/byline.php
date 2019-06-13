f179f508899d49aaadba359e9dc8ec571f0c184a
Andreas Wieden <andreas.wieden@chalmers.se>
2019-06-13 13:06:12 +0200

    added basic style to byline objects in byline.php

--- incl/byline.php
+++ incl/byline.php
@@ -1,7 +1,9 @@
 <div class="byline">
+  <span class="byline-img-holder">
+    <img src="img/me-small.jpg" alt="bild på Mikael Roos">
+    <figcaption>Mikael Roos</figcaption>
+  </span>
   
-  <img src="img/me-small.jpg" alt="bild på Mikael Roos">
-  <figcaption>Mikael Roos</figcaption>
   <span class="byline-text">
     Mikael Roos undervisar vid Blekinge Tekniska Högskola och skriver om webbprogrammering, webbutveckling och utmaningen med att undervisa på distans och campus. Mikael driver ett företag inom webbprogrammering och på kvällarna utvecklar han sina projekt inom öppen källkod.
   </span>
