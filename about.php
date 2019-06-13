b55c11fc9b92189e97c9821482455f0d24c23e5c
Andreas Wieden <andreas.wieden@chalmers.se>
2019-06-13 12:06:49 +0200

    included incl/byline.php in me.php, about.php and report.php

--- about.php
+++ about.php
@@ -5,7 +5,9 @@
   <h1>Om Denna Webbplats</h1>
   <p>Här kommer snart min egen fina me sida</p>
   <img src="img/me.jpg" class="me" alt="Bild på Roos">
-    
+  
+  <?php include("incl/byline.php");?>
+  
 <?php
   include("incl/footer.php");
 ?>
--- me.php
+++ me.php
@@ -3,7 +3,9 @@
   <h1>Om Mig Själv</h1>
   <p>Här kommer snart min egen fina me sida</p>
   <img src="img/me.jpg" class="me" alt="Bild på Roos">
-    
+  
+  <?php include("incl/byline.php");?>
+  
 <?php include("incl/footer.php"); ?>
 
 
